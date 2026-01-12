f28257cf5023e1bb9bad1ec1a0ebc47719e8ae0e
hunmer <liaoyanjie2000@gmail.com>
2026-01-12 08:52:46 +0800

    fix

--- lib/screens/widgets_gallery/screens/rounded_task_list_card_example.dart
+++ lib/screens/widgets_gallery/screens/rounded_task_list_card_example.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:animated_flip_counter/animated_flip_counter.dart';
 
 /// 圆角任务列表卡片示例
 class RoundedTaskListCardExample extends StatelessWidget {
