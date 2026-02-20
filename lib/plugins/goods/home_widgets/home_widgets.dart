a4162afa6bece7207ed621d4bc065068112d0fd4
hunmer <liaoyanjie2000@gmail.com>
2026-02-20 12:39:23 +0800

    fix

--- lib/plugins/goods/home_widgets/home_widgets.dart
+++ lib/plugins/goods/home_widgets/home_widgets.dart
@@ -3,7 +3,6 @@
 /// 提供多个主页小组件：
 /// - [registerIconWidget] - 1x1 简单图标组件
 /// - [registerOverviewWidget] - 2x2 详细卡片组件
-/// - [registerWarehouseSelectorWidget] - 仓库选择器组件
 /// - [registerItemSelectorWidget] - 物品选择器组件
 /// - [registerGoodsListWidget] - 物品列表组件（带过滤器）
 library;
@@ -15,14 +14,12 @@ export 'widgets.dart';
 // 注册文件
 export 'register_icon_widget.dart';
 export 'register_overview_widget.dart';
-export 'register_warehouse_selector.dart';
 export 'register_item_selector.dart';
 export 'register_goods_list_widget.dart';
 
 import 'package:Memento/screens/home_screen/managers/home_widget_registry.dart';
 import 'register_icon_widget.dart';
 import 'register_overview_widget.dart';
-import 'register_warehouse_selector.dart';
 import 'register_item_selector.dart';
 import 'register_goods_list_widget.dart';
 
@@ -31,7 +28,6 @@ void register() {
   final registry = HomeWidgetRegistry();
   registerIconWidget(registry);
   registerOverviewWidget(registry);
-  registerWarehouseSelectorWidget(registry);
   registerItemSelectorWidget(registry);
   registerGoodsListWidget(registry);
 }
--- lib/plugins/goods/home_widgets/widgets.dart
+++ lib/plugins/goods/home_widgets/widgets.dart
@@ -1,5 +1,4 @@
 /// 物品管理插件主页小组件导出
 library;
 
-export 'widgets/goods_warehouse_widget.dart';
 export 'widgets/goods_item_widget.dart';
