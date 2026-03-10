9d248f26195636f899e5bc159b458e2329ccc645
hunmer <liaoyanjie2000@gmail.com>
2026-03-11 00:29:34 +0800

    watchos

---
+++ ios/memento Watch App/mementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  mementoApp.swift
+//  memento Watch App
+//
+//  Created by liao on 2026/3/11.
+//
+
+import SwiftUI
+
+@main
+struct memento_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
