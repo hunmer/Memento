b653be63a9591734d549123c771fbd01918d18e3
hunmer <liaoyanjie2000@gmail.com>
2026-03-25 22:56:12 +0800

    fix

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,5 +1,4 @@
 #import "GeneratedPluginRegistrant.h"
 #import <Flutter/Flutter.h>
-#import <flutter_foreground_task/FlutterForegroundTaskPlugin.h>
 #import <UserNotifications/UserNotifications.h>
 #import <UIKit/UIKit.h>
